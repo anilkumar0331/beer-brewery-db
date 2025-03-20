3c9681dc93018f939f71615ff0841e8ad0e72ded
Anil <anilkumariz4u@gmail.com>
2025-03-20 18:30:58 -0500

    Modified name of the path

--- src/main/java/com/anilkumar/beer_brewery_db/controller/InventoryController.java
+++ src/main/java/com/anilkumar/beer_brewery_db/controller/InventoryController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/beer-inventory")
+@RequestMapping("/inventory")
 public class InventoryController {
 
     private InventoryService inventoryService;
