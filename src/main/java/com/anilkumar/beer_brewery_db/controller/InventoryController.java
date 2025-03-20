0a2b3e8a4fe1e4467e708ca9ea00d45213f63732
Anil <anilkumariz4u@gmail.com>
2025-03-20 17:45:56 -0500

    Modified API path

--- src/main/java/com/anilkumar/beer_brewery_db/controller/InventoryController.java
+++ src/main/java/com/anilkumar/beer_brewery_db/controller/InventoryController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/inventory")
+@RequestMapping("/beer-inventory")
 public class InventoryController {
 
     private InventoryService inventoryService;
